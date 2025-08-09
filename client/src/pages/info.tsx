09c9b80351de558e7366ab97e02ab24fea1af4be
drillvoice <joel.dignam@gmail.com>
2025-08-09 18:07:33 +0930

    Update info.tsx
    minor edits to info text.

--- client/src/pages/info.tsx
+++ client/src/pages/info.tsx
@@ -13,7 +13,7 @@ export default function Info() {
           <p className="text-sm text-gray-600">
             Pawn Star is an app to help you get the most out of your chess practice. Playing games and studying tactics is
             essential to improve at chess. And you can get even more out of it when you track your progress and review how you
-            are going. This approach is inspired by
+            are going. This approach is inspired by 
             <a href="https://nextlevelchess.com/free-ebook/" target="_blank">
               <em>The Art of Chess Training</em>
             </a>
@@ -28,9 +28,9 @@ export default function Info() {
         </CardHeader>
         <CardContent>
           <p className="text-sm text-gray-600">
-            I recently got back into chess after a long hiatus. 
+            I am OK at chess and OK at making apps! I recently got back into chess after a long hiatus.
             Not only do I love the game, I love the way it's possible to quantify my progress and break down my learning journey. 
-            This is my first app and I hope it helps chess students everywhere! On Lichess, I am <a href="https://lichess.org/@/softtalk" target="_blank">softtalk</a>. 
+            This is my first app and I hope it helps chess students everywhere! If you fancy a game, I am <a href="https://lichess.org/@/softtalk" target="_blank">softtalk on Lichess</a>. 
           </p>
         </CardContent>
       </Card>
@@ -43,7 +43,7 @@ export default function Info() {
           <p className="text-sm text-gray-600">
             This app is still in development. 
             I've been using it for weeks and it works for me, but I'm sure it could work better! 
-            If you have any feedback or comments,
+            If you have any feedback or comments, 
             <a
               className="text-blue-600 hover:underline"
               href="mailto:pawn.star.chess.logger@gmail.com?subject=Pawn%20Star%20Feedback"
