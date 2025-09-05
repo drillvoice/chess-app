db00805171624115836acac0165e49fd2f8b4caf
drillvoice <joel.dignam@gmail.com>
2025-09-05 13:51:20 +0930

    feat: log requests without res.json override

--- server/index.ts
+++ server/index.ts
@@ -2,40 +2,13 @@ import express, { type Request, Response, NextFunction, json, urlencoded } from 
 import { registerRoutes } from './routes';
 import { setupVite, serveStatic, log } from './vite';
 import { fromZodError } from 'zod-validation-error';
+import { requestLogger } from './logger';
 
 const app = express();
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  const start = Date.now();
-  const path = req.path;
-  let capturedJsonResponse: Record<string, any> | undefined;
-
-  const originalResJson = res.json;
-  res.json = function (bodyJson, ...args) {
-    capturedJsonResponse = bodyJson;
-    return originalResJson.apply(res, [bodyJson, ...args]);
-  };
-
-  res.on('finish', () => {
-    const duration = Date.now() - start;
-    if (path.startsWith('/api')) {
-      let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
-      if (capturedJsonResponse) {
-        logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
-      }
-
-      if (logLine.length > 80) {
-        logLine = logLine.slice(0, 79) + '…';
-      }
-
-      log(logLine);
-    }
-  });
-
-  next();
-});
+app.use(requestLogger);
 
 (async () => {
   const server = await registerRoutes(app);
